fc6bdafa88e25b9920b2b7e221a43961c4efdc38
Umberto Valleriani <umberto.valleriani@gmail.com>
2021-11-09 19:21:13 +0100

    Add simple feedforward nn 2

--- src/lit_models.py
+++ src/lit_models.py
@@ -24,6 +24,89 @@ def compute_accuracy(logits, targets):
     )
 
 
+class MLPClassifierModel(pl.LightningModule):
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__()
+        self.save_hyperparameters()  # populate self.hparams with args and kwargs automagically!
+
+        self.model = hydra.utils.instantiate(
+            self.hparams.model, _recursive_=True, _convert_="partial"
+        )
+
+        self.classification_loss_fn = hydra.utils.instantiate(
+            self.hparams.classification_loss_fn
+        )
+
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        continuous_targets: torch.Tensor = None,
+        categorical_targets: torch.Tensor = None,
+    ):
+        prediction_logits = self.model(inputs)
+
+        out = {
+            "logits": prediction_logits,
+        }
+
+        if categorical_targets is not None:
+            classification_loss = self.classification_loss_fn(
+                prediction_logits, categorical_targets.view(-1)
+            )
+            out["clf_loss"] = classification_loss
+            out["categorical_targets"] = categorical_targets
+            out["loss"] = classification_loss
+            out["accuracy"] = compute_accuracy(prediction_logits, categorical_targets)
+
+        return out
+
+    def training_step(self, batch, batch_idx):
+        step_result = self.forward(*batch)
+        self.log_dict(
+            {
+                "train/clf_loss": step_result["clf_loss"],
+                "train/loss": step_result["loss"],
+                "train/accuracy": step_result["accuracy"],
+            },
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+        return step_result
+
+    def validation_step(self, batch, batch_idx):
+        step_result = self.forward(*batch)
+        self.log_dict(
+            {
+                "val/clf_loss": step_result["clf_loss"],
+                "val/loss": step_result["loss"],
+                "val/accuracy": step_result["accuracy"],
+            },
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+        )
+        return step_result
+
+    def configure_optimizers(
+        self,
+    ) -> Union[Optimizer, Tuple[Sequence[Optimizer], Sequence[Any]]]:
+        opt = hydra.utils.instantiate(
+            self.hparams.optim.optimizer, params=self.parameters(), _convert_="partial"
+        )
+        if self.hparams.optim.use_lr_scheduler:
+            scheduler = hydra.utils.instantiate(
+                self.hparams.optim.lr_scheduler, optimizer=opt
+            )
+            return [opt], [scheduler]
+
+        return [opt]
+
+
 class AutoEncoderModel(pl.LightningModule):
     def __init__(
         self,
@@ -143,6 +226,9 @@ def main(cfg: omegaconf.DictConfig):
         # logging=cfg.logging,
         _recursive_=False,
     )
+    in_size = len(cfg.dataset_conf.input_columns)
+    example_tensor = torch.randn(2, in_size)
+    example_output = model(example_tensor)
 
 
 if __name__ == "__main__":
--- src/models.py
+++ src/models.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
+from typing import Optional
+
 
 def NonLinear(
     in_size: int,
@@ -16,6 +18,34 @@ def NonLinear(
     )
 
 
+class SimpleFeedForward(nn.Module):
+    def __init__(
+        self,
+        in_size: int,
+        hidden_sizes: list[int],
+        activation: nn.Module = nn.Sigmoid(),
+        dropout: float = 0.0,
+    ):
+        super().__init__()
+        self.in_size = in_size
+        self.hidden_sizes = hidden_sizes
+        self.activation = activation
+        self.dropout = dropout
+
+        # list of tuples of adjacent layer sizes
+        projection_sizes = list(zip([in_size] + hidden_sizes, hidden_sizes))
+
+        self.net = nn.Sequential(
+            *[
+                NonLinear(s1, s2, activation=self.activation, dropout=self.dropout)
+                for (s1, s2) in projection_sizes
+            ]
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
 class AutoEncoder(nn.Module):
     def __init__(
         self,
@@ -28,27 +58,39 @@ class AutoEncoder(nn.Module):
         self.hidden_sizes = hidden_sizes
         self.activation = activation
 
-        # list of tuples of adjacent layer sizes
-        projection_sizes = list(zip([in_size] + hidden_sizes, hidden_sizes))
-
-        self.encoder = nn.Sequential(
-            *[
-                NonLinear(s1, s2, activation=self.activation)
-                for (s1, s2) in projection_sizes
-            ]
-        )
-
-        self.decoder = nn.Sequential(
-            *[
-                NonLinear(s2, s1, activation=self.activation)
-                for (s1, s2) in reversed(projection_sizes)
-            ]
+        self.encoder = SimpleFeedForward(in_size, hidden_sizes, activation)
+        decoder_hidden_sizes = list(reversed(hidden_sizes)) + [in_size]
+        self.decoder = SimpleFeedForward(
+            hidden_sizes[-1], decoder_hidden_sizes, activation
         )
 
     def forward(self, x) -> tuple[torch.Tensor, torch.Tensor]:
         encoded = self.encoder(x)
         reconstruction = self.decoder(encoded)
         return encoded, reconstruction
+
+
+class Classifier(nn.Module):
+    def __init__(
+        self,
+        feature_extractor: SimpleFeedForward,
+        n_classes: int,
+        feature_dim: Optional[int] = None,
+    ):
+        super().__init__()
+        feature_dim = (
+            feature_dim
+            if feature_dim is not None
+            else feature_extractor.hidden_sizes[-1]
+        )
+        self.feature_extractor = feature_extractor
+        self.n_classes = n_classes
+        self.feature_dim = feature_dim
+        self.classifier = nn.Linear(feature_dim, n_classes)
+
+    def forward(self, x):
+        features = self.feature_extractor(x)
+        return self.classifier(features)
 
 
 class ClassifierWithAutoEncoder(nn.Module):
